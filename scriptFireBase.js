2945e75578957794322b18c9d6f7d0f4f261cc8e
redd <daniele.coppa30@gmail.com>
2024-05-22 14:52:25 +0200

    update

--- scriptFireBase.js
+++ scriptFireBase.js
@@ -1,5 +1,7 @@
 // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-app.js";
+  import {getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-auth.js";
+  
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,12 +19,24 @@
   const app = initializeApp(firebaseConfig);
 
   //inputs
-  const username = document.getElementById("username").value;
+  const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
 
   //submit button
   const submit = document.getElementById("submit");
   submit.addEventListener("click",function (event) {
     event.preventDefault();
-    alert("5")
+    createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed up 
+    const user = userCredential.user;
+    alert("Creazione Account In Corso...")
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    alert(errorMessage)
+    // ..
+  });
   })
