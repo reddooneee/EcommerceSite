70c55e0b690398a06904f4c823c47ae54bc9ac5b
reddooneee <daniele.coppa30@gmail.com>
2024-06-03 21:13:12 +0200

    New Update

---
+++ Calcio/scriptCart.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const button = document.getElementById("btn-cart");
+    button.addEventListener("click", () => {
+      alert("Spiacenti ma il prodotto selezionato ancora non è disponibile");
+    });
+  });
